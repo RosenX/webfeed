c263e07c3fb5faabc00a978dc0d3c584ef3b923f
luosen <heyrosen@163.com>
2023-06-13 11:10:58 +0800

    在pubdate中去除多余空格,修复时间解析问题

--- lib/util/datetime.dart
+++ lib/util/datetime.dart
@@ -4,13 +4,18 @@ const rfc822DatePattern = 'EEE, dd MMM yyyy HH:mm:ss Z';
 
 DateTime? parseDateTime(dateString) {
   if (dateString == null) return null;
-  return _parseRfc822DateTime(dateString) ?? _parseIso8601DateTime(dateString);
+  String cleanedString = dateString.trim().replaceAll(RegExp(' +'), ' ');
+  return _parseRfc822DateTime(cleanedString) ??
+      _parseIso8601DateTime(cleanedString);
 }
 
 DateTime? _parseRfc822DateTime(String dateString) {
   try {
-    final num? length = dateString.length.clamp(0, rfc822DatePattern.length);
-    final trimmedPattern = rfc822DatePattern.substring(0, length as int?); //Some feeds use a shortened RFC 822 date, e.g. 'Tue, 04 Aug 2020'
+    final num length = dateString.length.clamp(0, rfc822DatePattern.length);
+    final trimmedPattern = rfc822DatePattern.substring(
+        0,
+        length
+            as int?); //Some feeds use a shortened RFC 822 date, e.g. 'Tue, 04 Aug 2020'
     final format = DateFormat(trimmedPattern, 'en_US');
     return format.parse(dateString);
   } on FormatException {
